9471e9c06769bee07d8ffc2aa3a77618697bdda2
Kristina Vlasovets <kristinavlasovets@gmail.com>
2022-11-29 15:25:52 +0300

    changed PORT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const io = require('socket.io')(8900, {
+const PORT = process.env.PORT || 4000;
+
+const io = require('socket.io')(PORT, {
 	cors: {
 		origin: process.env.CLIENT_URL,
 	},
